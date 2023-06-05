2331777d85176c228d3d5ef60a0dddef8ffc392c
Alfredo Manuel <alfredomanuel127.0@gmail.com>
2023-06-05 16:05:30 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './style.css'
+import './components/Header'
+import Header from './components/Header'
+import Section from './components/Section'
+
+function App() {
+return (
+ 
+  <div className='App'>
+    <Header/>
+    <main>
+    <Section/>
+  </main>
+  </div>
+
+  )
+}
+
+export default App
